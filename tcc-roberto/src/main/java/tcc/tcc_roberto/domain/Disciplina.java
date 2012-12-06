dae73f815ea4c26915aadbaff9ed713437a7e4b5
jribacruz <jribacruz@gmail.com>
2012-12-06 13:48:44 -0300

    Classe Disciplina

---
+++ tcc-roberto/src/main/java/tcc/tcc_roberto/domain/Disciplina.java
@@ -0,0 +1,87 @@
+package tcc.tcc_roberto.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Disciplina implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column
+	private String descricao;
+	@Column
+	private Integer cargaHoraria;
+
+	public Disciplina() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getCargaHoraria() {
+		return cargaHoraria;
+	}
+
+	public void setCargaHoraria(Integer cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cargaHoraria == null) ? 0 : cargaHoraria.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Disciplina other = (Disciplina) obj;
+		if (cargaHoraria == null) {
+			if (other.cargaHoraria != null)
+				return false;
+		} else if (!cargaHoraria.equals(other.cargaHoraria))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Disciplina [id=" + id + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+	}
+
+}
